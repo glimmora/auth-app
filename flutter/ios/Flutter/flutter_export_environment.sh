7fe00f5cfc6a62b410e9dea62b10cfd8a298828b
glimmora <bukanartis777@gmail.com>
2026-04-01 16:37:58 +0200

    1

--- flutter/ios/Flutter/flutter_export_environment.sh
+++ flutter/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Apps\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\dev\auth-app\flutter"
+export "FLUTTER_ROOT=/home/blue/sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/dev/vscode-projects/blue/projects/auth-app/flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
